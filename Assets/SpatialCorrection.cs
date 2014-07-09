f0e76bfd099da2bc3a48ecf5f3d9d1cc342144b7
Ian Nastajus <ian.nastajus@gmail.com>
2014-07-09 14:51:36 -0400

    basic project setup

---
+++ Assets/SpatialCorrection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SpatialCorrection : MonoBehaviour {
+
+    public const int blockSize = 4;
+    
+
+    public enum Orientation { Horizontal, Vertical };
+    public Orientation orientation = Orientation.Horizontal; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (orientation == Orientation.Horizontal){
+            transform.position = new Vector3(
+                ((int)Mathf.Round((transform.position.x - blockSize / 2) / blockSize)) * blockSize + blockSize / 2,
+                ((int)Mathf.Round(transform.position.y / blockSize)) * blockSize,
+                transform.position.z );
+        }
+
+        else if (orientation == Orientation.Vertical)
+        {
+            transform.position = new Vector3(
+                ((int)Mathf.Round(transform.position.x / blockSize)) * blockSize,
+                ((int)Mathf.Round((transform.position.y - blockSize / 2) / blockSize)) * blockSize + blockSize / 2,
+                transform.position.z);
+        }
+	}
+}
