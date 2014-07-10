7e6b42e5c7de13dfa1563738d99a65ebedb7808c
Ian Nastajus <ian.nastajus@gmail.com>
2014-07-09 22:31:19 -0400

    made Resources folder with Prefabs

---
+++ Assets/Author.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Author : MonoBehaviour {
+
+    public enum difficulty_setting { Easy, Normal, Hard, Nightmare, Battletoads }
+
+    public string author;
+    public string levelName;
+    public difficulty_setting difficulty;
+
+}
