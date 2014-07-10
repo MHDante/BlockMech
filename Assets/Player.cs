efb80e60492fd9eb0d66e5b3e3da2cfe4a874f54
Ian Nastajus <ian.nastajus@gmail.com>
2014-07-09 22:16:28 -0400

    ian setup tags & player movement

---
+++ Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    int moveCounter = 0;
+    int distance = 4;
+    float timer;
+    float moveDelayTime = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+        GameObject start = GameObject.FindGameObjectWithTag("Start");
+        if (start != null) {
+            transform.position = start.transform.position;
+        }
+        else
+        {
+            Debug.Log("Player requries a start prefab to exist.");
+        }
+        timer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x, y;
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        float deltaTick = Time.time - timer;
+
+        if (deltaTick > moveDelayTime)
+        {
+            timer += deltaTick;
+        }
+
+        if (x != 0 || y != 0)
+        {
+            transform.position = new Vector3(
+                transform.position.x + x * distance, 
+                transform.position.y + y * distance, 
+                transform.position.z
+             );
+        }
+    }
+}
