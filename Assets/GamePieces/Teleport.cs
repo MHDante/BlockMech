e1e34bcfbde39c018a82a28184835414a100c85e
MHDante <mhdcj@hotmail.com>
2014-07-14 20:00:04 -0400

    Missing assts

--- Assets/GamePieces/Teleport.cs
+++ Assets/GamePieces/Teleport.cs
@@ -6,7 +6,6 @@ public class Teleport : GamePiece
     public override bool isSolid { get { return true; } set { } }
     public override bool isPushable { get { return true; } set { } }
     public GameObject target;
-
     public override void Start()
     {
         base.Start();
@@ -21,5 +20,4 @@ public class Teleport : GamePiece
         //piece.TeleportTo(target.GetComponent<GamePiece>().cell);
         return base.onOccupy(piece);
     }
-
 }
